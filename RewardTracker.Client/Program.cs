56832a6a81f24c905156c07543008a94a81352a5
Milan Milic <milan.milic@swiss.com>
2026-08-29 13:04:17 +0200

    Uklonjeni backtickovi iz Uri poziva

--- RewardTracker.Client/Program.cs
+++ RewardTracker.Client/Program.cs
@@ -6,7 +6,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(`"https://localhost:7214/`") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7214/") });
 
 await builder.Build().RunAsync();
 

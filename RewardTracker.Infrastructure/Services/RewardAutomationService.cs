7dea803bd92d9e267bfd09611666948e30a0c6e5
Milan Milic <milan.milic@swiss.com>
2026-08-30 11:56:46 +0200

    Optimizacija pretraga: uklonjen mobilni bot i smanjen broj pretraga za Srbiju

--- RewardTracker.Infrastructure/Services/RewardAutomationService.cs
+++ RewardTracker.Infrastructure/Services/RewardAutomationService.cs
@@ -20,7 +20,6 @@ public class RewardAutomationService
         _backgroundJobs = backgroundJobs;
     }
 
-    // Glavni zakazivac (okida se npr. svako jutro u 06:00 preko Hangfire-a)
     public void ScheduleRandomDailyRuns()
     {
         using var scope = _serviceProvider.CreateScope();
@@ -31,16 +30,11 @@ public class RewardAutomationService
 
         foreach (var account in activeAccounts)
         {
-            // Nasumicno vreme pokretanja izmedju 5 i 120 minuta od trenutka zakazivanja
-            // Da Microsoft ne bi primetio fiksno okidanje u minut
             var randomDelayMinutes = random.Next(5, 120);
-            
             _backgroundJobs.Schedule<RewardAutomationService>(
                 s => s.RunDailyTasksAsync(account.Id), 
                 TimeSpan.FromMinutes(randomDelayMinutes)
             );
-            
-            Console.WriteLine($"Nalog ID {account.Id} zakazan za pokretanje za {randomDelayMinutes} minuta.");
         }
     }
 
@@ -82,28 +76,26 @@ public class RewardAutomationService
         var random = new Random();
         var baseWords = new[] { "Srbija", "Beograd", "Vesti", "Sport", "Filmovi", "Recepti", "Tehnologija", "Zanimljivosti", "Istorija", "Automobili", "Kompjuteri", "Muzika", "Klima", "Putovanja", "Fizika", "Astronomija", "Planete", "Ekonomija", "Zdravlje", "Trening", "Ishrana", "Programiranje", "Arhitektura" };
 
-        Console.WriteLine("=== START: PC PRETRAGE ===");
+        Console.WriteLine("=== START: BING PRETRAGE (Optimizovano za Srbiju) ===");
         var desktopOptions = new BrowserNewContextOptions { StorageState = account.SessionData };
         var desktopContext = await browser.NewContextAsync(desktopOptions);
         var desktopPage = await desktopContext.NewPageAsync();
 
         try 
         {
-            // Radimo 35 pretraga za PC (Maksimum za poene je obicno 30)
-            for(int i = 0; i < 35; i++)
+            // Smanjeno na 25 pretraga ukupno (Dovoljno za 60 poena = 20 pretraga + malo bafera)
+            for(int i = 0; i < 25; i++)
             {
                 var term = baseWords[random.Next(baseWords.Length)] + " " + baseWords[random.Next(baseWords.Length)] + " " + random.Next(100, 9999);
                 await desktopPage.GotoAsync("https://www.bing.com");
                 
-                // Cekamo malo da strana bude spremna
                 await Task.Delay(2000);
                 
                 var searchInput = desktopPage.Locator("[name='q']").First;
                 await searchInput.FillAsync(term, new() { Force = true });
                 await searchInput.PressAsync("Enter");
                 
-                // Random pauza izmedju pretraga (od 4 do 12 sekundi - zvuci sasvim prirodno)
-                await Task.Delay(random.Next(4000, 12000)); 
+                await Task.Delay(random.Next(4000, 10000)); 
             }
 
             Console.WriteLine("=== CITANJE UKUPNIH POENA SA BING EKRANA ===");
@@ -127,7 +119,6 @@ public class RewardAutomationService
                 if (int.TryParse(cleanNumber, out int pts) && pts > 0)
                 {
                     Console.WriteLine("Uspesno prepoznat broj: " + pts);
-                    
                     var log = new RewardTracker.Core.Entities.PointLog
                     {
                         AccountId = account.Id,
@@ -143,42 +134,12 @@ public class RewardAutomationService
                 }
             }
             
-            // Cuvamo osvezenu sesiju sa novim kolacicima
             account.SessionData = await desktopContext.StorageStateAsync();
             dbContext.Accounts.Update(account);
             await dbContext.SaveChangesAsync();
         }
         catch (Exception ex) { Console.WriteLine("Greska PC: " + ex.Message); }
         finally { await desktopContext.CloseAsync(); }
-
-        Console.WriteLine("=== START: MOBILNE PRETRAGE ===");
-        var mobileOptions = playwright.Devices["Pixel 5"];
-        mobileOptions.StorageState = account.SessionData; 
-        
-        var mobileContext = await browser.NewContextAsync(mobileOptions);
-        var mobilePage = await mobileContext.NewPageAsync();
-
-        try 
-        {
-            // Radimo 25 pretraga za Mobilni (Maksimum za poene je 20)
-            for(int i = 0; i < 25; i++)
-            {
-                var term = baseWords[random.Next(baseWords.Length)] + " " + baseWords[random.Next(baseWords.Length)] + " " + random.Next(100, 9999);
-                await mobilePage.GotoAsync("https://www.bing.com");
-                
-                await Task.Delay(2000);
-                
-                // Mobilna verzija cesto ima drugaciji ID za pretragu, ali name='q' obicno radi. 
-                // Koristimo Force = true da ignorisemo eventualne popup-ove koji ga prekrivaju
-                var searchInput = mobilePage.Locator("[name='q']").First;
-                await searchInput.FillAsync(term, new() { Force = true });
-                await searchInput.PressAsync("Enter");
-                
-                await Task.Delay(random.Next(4000, 12000));
-            }
-        }
-        catch (Exception ex) { Console.WriteLine("Greska Mobilni: " + ex.Message); }
-        finally { await mobileContext.CloseAsync(); }
         
         Console.WriteLine("=== BOT JE ZAVRSIO SA RADOM ===");
     }
